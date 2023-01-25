3d1489401c2fae21bb71fd73dd42b7465044b91f
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-26 00:05:44 +0100

    quick fix : Collections.sort() can be replaced with List.sort()

--- src/main/java/tconstruct/util/network/PacketPipeline.java
+++ src/main/java/tconstruct/util/network/PacketPipeline.java
@@ -137,19 +137,13 @@ public class PacketPipeline extends MessageToMessageCodec<FMLProxyPacket, Abstra
         if (this.isPostInitialised) {
             return;
         }
-
         this.isPostInitialised = true;
-        Collections.sort(this.packets, new Comparator<Class<? extends AbstractPacket>>() {
-
-            @Override
-            public int compare(Class<? extends AbstractPacket> clazz1, Class<? extends AbstractPacket> clazz2) {
-                int com = String.CASE_INSENSITIVE_ORDER.compare(clazz1.getCanonicalName(), clazz2.getCanonicalName());
-                if (com == 0) {
-                    com = clazz1.getCanonicalName().compareTo(clazz2.getCanonicalName());
-                }
-
-                return com;
+        this.packets.sort((clazz1, clazz2) -> {
+            int com = String.CASE_INSENSITIVE_ORDER.compare(clazz1.getCanonicalName(), clazz2.getCanonicalName());
+            if (com == 0) {
+                com = clazz1.getCanonicalName().compareTo(clazz2.getCanonicalName());
             }
+            return com;
         });
     }
 
