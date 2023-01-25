8b976cf0546058de38c046c435847605a62918fc
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-25 21:10:05 +0100

    quick fix : Unnecessary interface modifier

--- src/main/java/tconstruct/library/accessory/IHealthAccessory.java
+++ src/main/java/tconstruct/library/accessory/IHealthAccessory.java
@@ -3,5 +3,5 @@ package tconstruct.library.accessory;
 import net.minecraft.item.ItemStack;
 
 public interface IHealthAccessory extends IAccessory {
-    public int getHealthBoost(ItemStack item);
+    int getHealthBoost(ItemStack item);
 }
--- src/main/java/tconstruct/library/weaponry/IAccuracy.java
+++ src/main/java/tconstruct/library/weaponry/IAccuracy.java
@@ -3,9 +3,9 @@ package tconstruct.library.weaponry;
 import net.minecraft.item.ItemStack;
 
 public interface IAccuracy {
-    public float minAccuracy(ItemStack itemStack);
+    float minAccuracy(ItemStack itemStack);
 
-    public float maxAccuracy(ItemStack itemStack);
+    float maxAccuracy(ItemStack itemStack);
 
-    public float getAccuracy(ItemStack itemStack, int time);
+    float getAccuracy(ItemStack itemStack, int time);
 }
