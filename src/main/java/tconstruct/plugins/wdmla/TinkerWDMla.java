6ee934875ac55db68f65f1f9db05bac316aba368
Quarri6343 <kaito6343@gmail.com>
2025-04-12 22:25:57 +0900

    supports probably all unsupported TiC storages

--- src/main/java/tconstruct/plugins/wdmla/TinkerWDMla.java
+++ src/main/java/tconstruct/plugins/wdmla/TinkerWDMla.java
@@ -1,5 +1,7 @@
 package tconstruct.plugins.wdmla;
 
+import com.gtnewhorizons.wdmla.plugin.universal.ItemStorageProvider;
+import mantle.blocks.abstracts.InventorySlab;
 import net.minecraft.util.ResourceLocation;
 
 import com.gtnewhorizons.wdmla.api.IWDMlaClientRegistration;
@@ -12,7 +14,9 @@ import mantle.pulsar.pulse.Pulse;
 import tconstruct.TConstruct;
 import tconstruct.armor.blocks.DryingRack;
 import tconstruct.mechworks.blocks.BlockLandmine;
+import tconstruct.tools.blocks.CraftingStationBlock;
 import tconstruct.tools.blocks.FurnaceSlab;
+import tconstruct.tools.blocks.ToolStationBlock;
 
 @Pulse(
         id = "Tinkers WDMla Compatibility",
@@ -27,6 +31,9 @@ public class TinkerWDMla implements IWDMlaPlugin {
         registration.registerBlockDataProvider(DryingRackProvider.INSTANCE, DryingRack.class);
         registration.registerBlockDataProvider(TECustomNameHeaderProvider.INSTANCE, FurnaceSlab.class);
         registration.registerBlockDataProvider(FurnaceSlabProvider.INSTANCE, FurnaceSlab.class);
+        registration.registerItemStorage(ItemStorageProvider.Extension.INSTANCE, CraftingStationBlock.class);
+        registration.registerItemStorage(ItemStorageProvider.Extension.INSTANCE, ToolStationBlock.class);
+        registration.registerItemStorage(ItemStorageProvider.Extension.INSTANCE, InventorySlab.class);
     }
 
     @Override
