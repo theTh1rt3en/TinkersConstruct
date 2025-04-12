a18bc1485e40e07ab8c98b35dc7d631601c960b7
Quarri6343 <kaito6343@gmail.com>
2025-04-12 15:20:46 +0900

    LandmineHeaderProvider

---
+++ src/main/java/tconstruct/plugins/wdmla/LandmineHeaderProvider.java
@@ -0,0 +1,29 @@
+package tconstruct.plugins.wdmla;
+
+import com.gtnewhorizons.wdmla.api.accessor.BlockAccessor;
+import com.gtnewhorizons.wdmla.api.provider.IBlockComponentProvider;
+import com.gtnewhorizons.wdmla.api.ui.ITooltip;
+import com.gtnewhorizons.wdmla.impl.ui.ThemeHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import tconstruct.TConstruct;
+import tconstruct.mechworks.logic.TileEntityLandmine;
+
+public enum LandmineHeaderProvider implements IBlockComponentProvider {
+    INSTANCE;
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor accessor) {
+        if(accessor.getTileEntity() instanceof TileEntityLandmine landmine) {
+            ItemStack cover = landmine.getStackInSlot(3);
+            if(cover != null) {
+                ThemeHelper.INSTANCE.overrideTooltipHeader(tooltip, cover);
+            }
+        }
+    }
+
+    @Override
+    public ResourceLocation getUid() {
+        return new ResourceLocation(TConstruct.modID, "landmine_header");
+    }
+}
---
+++ src/main/java/tconstruct/plugins/wdmla/TinkerWDMla.java
@@ -0,0 +1,26 @@
+package tconstruct.plugins.wdmla;
+
+import com.gtnewhorizons.wdmla.api.IWDMlaClientRegistration;
+import com.gtnewhorizons.wdmla.api.IWDMlaCommonRegistration;
+import com.gtnewhorizons.wdmla.api.IWDMlaPlugin;
+import com.gtnewhorizons.wdmla.api.WDMlaPlugin;
+import mantle.pulsar.pulse.Pulse;
+import tconstruct.mechworks.blocks.BlockLandmine;
+
+@Pulse(
+        id = "Tinkers WDMla Compatibility",
+        description = "Tinkers Construct compatibility for WDMla",
+        modsRequired = "wdmla",
+        forced = true)
+@WDMlaPlugin
+public class TinkerWDMla implements IWDMlaPlugin {
+
+    @Override
+    public void register(IWDMlaCommonRegistration registration) {
+    }
+
+    @Override
+    public void registerClient(IWDMlaClientRegistration registration) {
+        registration.registerBlockComponent(LandmineHeaderProvider.INSTANCE, BlockLandmine.class);
+    }
+}
